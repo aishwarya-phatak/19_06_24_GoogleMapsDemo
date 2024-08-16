536f45dfabd3e347c7485adbaf6cfbbeba645777
aishwarya <ashp17star@gmail.com>
2024-08-16 12:14:14 +0530

    GMSMapViewDelegate Protocol implementation with GoogleMaps

--- 19_06_24_GoogleMapsDemo/ViewController.swift
+++ 19_06_24_GoogleMapsDemo/ViewController.swift
@@ -28,6 +28,7 @@ class ViewController: UIViewController {
     var gmsCircle : GMSCircle?
     var gmsPolygon : GMSPolygon?
     var gmsPolyline : GMSPolyline?
+    var infoWindowView : UIView?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -49,11 +50,13 @@ class ViewController: UIViewController {
         marker?.title = title
         marker?.opacity = 1.0
         marker?.snippet = "This is \(title) marker"
-        marker?.icon = UIImage(named: "city_icon")
+//        marker?.icon = UIImage(named: "city_icon")
+        marker?.tracksInfoWindowChanges = true
         marker?.map = mapView
     }
     
     private func initializeMapSettings(){
+        mapView.delegate = self
         mapView.settings.compassButton = true
         mapView.settings.myLocationButton = true
         mapView.settings.rotateGestures = true
@@ -90,7 +93,7 @@ class ViewController: UIViewController {
         gmsPolygon.strokeWidth = 7.0
         gmsPolygon.map = mapView
     }
-    
+        
     private func drawPolyline(){
         let gmsPathForPolyline = GMSMutablePath()
         gmsPathForPolyline.add(CLLocationCoordinate2D(latitude: 19.0760, longitude: 72.877))
@@ -103,5 +106,77 @@ class ViewController: UIViewController {
         gmsPolyline.strokeColor = .blue
         gmsPolyline.strokeWidth = 7.0
         gmsPolyline.map = mapView
+    }
+}
+
+extension ViewController : GMSMapViewDelegate{
+    
+    func mapView(_ mapView: GMSMapView, markerInfoWindow marker: GMSMarker) -> UIView? {
+        let cgRectCoordinatesForInfoWindow = CGRect(x: 0, y: 0, width: 200, height: 160)
+        infoWindowView = UIView(frame: cgRectCoordinatesForInfoWindow)
+        infoWindowView!.backgroundColor = .lightGray
+        infoWindowView!.layer.borderColor = CGColor(red: 100.0, green: 100.0, blue: 0.0, alpha: 4.0)
+        infoWindowView!.layer.borderWidth = 6.0
+        
+        let cgRectForLabelOne = CGRect(x: 20, y: 20, width: 100, height: 50)
+        let labelOne = UILabel(frame: cgRectForLabelOne)
+        labelOne.layer.borderColor = CGColor(red: 100.0, green: 100.0, blue: 200.0, alpha: 5.0)
+        labelOne.layer.borderWidth = 6.0
+        labelOne.backgroundColor = .white
+        labelOne.textColor = .black
+        labelOne.textAlignment = .center
+        labelOne.text = "\(marker.title!)"
+        
+        let cgRectForLabelTwo = CGRect(x: 20, y: 90, width: 100, height: 50)
+        let labelTwo = UILabel(frame: cgRectForLabelTwo)
+        labelTwo.layer.borderColor = CGColor(red: 100.0, green: 100.0, blue: 200.0, alpha: 5.0)
+        labelTwo.layer.borderWidth = 6.0
+        labelTwo.backgroundColor = .white
+        labelTwo.textAlignment = .center
+        labelTwo.textColor = .black
+        labelTwo.text = "Enjoy"
+        
+        infoWindowView!.addSubview(labelOne)
+        infoWindowView!.addSubview(labelTwo)
+        
+        return infoWindowView
+    }
+    
+    func mapView(_ mapView: GMSMapView, markerInfoContents marker: GMSMarker) -> UIView? {
+        return infoWindowView
+    }
+
+    //important in terms of displaying info Window
+    func mapView(_ mapView: GMSMapView, didTap marker: GMSMarker) -> Bool {
+        print("DidTap --- \(marker.title)")
+        return false
+    }
+    
+    func mapView(_ mapView: GMSMapView, didTapInfoWindowOf marker: GMSMarker) {
+        print("Marker position -- \(marker.title) -- \(marker.position.latitude) -- \(marker.position.longitude)")
+    }
+    
+    func mapView(_ mapView: GMSMapView, didTapAt coordinate: CLLocationCoordinate2D) {
+        print("Coordinate position -- -- \(coordinate.latitude) -- \(coordinate.longitude)")
+    }
+    
+    func mapView(_ mapView: GMSMapView, didDrag marker: GMSMarker) {
+        print("Did drag ----- Marker position -- \(marker.title) -- \(marker.position.latitude) -- \(marker.position.longitude)")
+    }
+    
+    func mapView(_ mapView: GMSMapView, didBeginDragging marker: GMSMarker) {
+        print("Did begin dragging  ---- Marker position -- \(marker.title) -- \(marker.position.latitude) -- \(marker.position.longitude)")
+    }
+    
+    func mapView(_ mapView: GMSMapView, didEndDragging marker: GMSMarker) {
+        print("Did End Dragging ---- Marker position -- \(marker.title) -- \(marker.position.latitude) -- \(marker.position.longitude)")
+    }
+    
+    func mapView(_ mapView: GMSMapView, didLongPressInfoWindowOf marker: GMSMarker) {
+        print("DidlongPressInfoWindowOf ---- Marker position -- \(marker.title) -- \(marker.position.latitude) -- \(marker.position.longitude)")
+    }
+    
+    func mapView(_ mapView: GMSMapView, didCloseInfoWindowOf marker: GMSMarker) {
+        print(marker.title)
     }
 }
